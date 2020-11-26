298a60f28f2be97c21dd84a848f220c031deee1b
manuel <manuel@sahores.com>
2020-11-26 17:54:46 +0100

    Story within story

---
+++ src/components/SmallForm/SmallForm.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Medium } from '../Input/Input.stories'
+import { Secondary } from '../Button/Button.stories'
+
+export default {
+  title: 'form/SmallForm'
+}
+
+export const SmallForm = () => (
+  <>
+		<Medium/>
+		<Secondary>Submit</Secondary>
+  </>
+)
