79bff8d905cd4a2816b02c7b58c03bcb754c7606
manuel <manuel@sahores.com>
2020-11-28 16:16:25 +0100

    Actions addon

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,32 +2,32 @@ import React from 'react'
 import Button from './Button'
 
 export default {
-  title: 'form/Button', 
-  component: Button
+  title: 'form/Button',
+  component: Button,
+  argTypes: { onClick: { action: 'clicked' } },
+	parameters: {
+    actions: {
+      handles: ['mouseover', 'click .btn'],
+    },
+  },
 }
-
-export const Primary = () => <Button variant='primary'>Primary</Button>
-export const Secondary = () => <Button variant='secondary'>Secondary</Button>
-export const Success = () => <Button variant='success'>Sucess</Button>
-export const Danger = () => <Button variant='danger'>Danger</Button>
 
 const Template = args => <Button {...args} />
 
-export const PrimaryA = Template.bind({})
-PrimaryA.args = {
+export const Primary = Template.bind({})
+Primary.args = {
   variant: 'primary',
-  children: 'Primary Args'
+  children: 'Primary'
 }
 
-export const PrimaryB = Template.bind({})
-PrimaryB.args = {
+export const Secondary = Template.bind({})
+Secondary.args = {
   variant: 'secondary',
   children: 'Secondary Args'
 }
 
-//reuse args from other story:
-export const LongPrimaryA = Template.bind({})
-LongPrimaryA.args = {
-  ...PrimaryA.args,
-  children: 'Long Primary Args'
+export const Danger = Template.bind({})
+Danger.args = {
+  variant: 'danger',
+  children: 'Danger'
 }
