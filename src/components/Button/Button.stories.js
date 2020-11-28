5235e94509e0eefa82eb685414386bcb5be72600
toshiba <manuel@sahores.com>
2020-11-28 11:01:39 +0100

    Using args in V6

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -10,3 +10,26 @@ export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Sucess</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+const Template = args => <Button {...args} />
+
+export const PrimaryA = Template.bind({})
+PrimaryA.args = {
+  variant: 'primary',
+  children: 'Primary Args'
+}
+
+export const PrimaryB = Template.bind({})
+PrimaryB.args = {
+  variant: 'secondary',
+  children: 'Secondary Args'
+}
+
+//reuse args from other story:
+export const LongPrimaryA = Template.bind({})
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  children: 'Long Primary Args'
+}
+
+
