9b430f94427f24b75a2aa607a4f049cb729efe9c
toshiba <manuel@sahores.com>
2020-11-28 11:04:34 +0100

    Args at a component level

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,7 +3,10 @@ import Button from './Button'
 
 export default {
   title: 'form/Button', 
-  component: Button
+  component: Button,
+  args: {
+    children: 'Button'
+  }
 }
 
 export const Primary = () => <Button variant='primary'>Primary</Button>
@@ -16,20 +19,20 @@ const Template = args => <Button {...args} />
 export const PrimaryA = Template.bind({})
 PrimaryA.args = {
   variant: 'primary',
-  children: 'Primary Args'
+  //children: 'Primary Args'
 }
 
 export const PrimaryB = Template.bind({})
 PrimaryB.args = {
   variant: 'secondary',
-  children: 'Secondary Args'
+  //children: 'Secondary Args'
 }
 
 //reuse args from other story:
 export const LongPrimaryA = Template.bind({})
 LongPrimaryA.args = {
   ...PrimaryA.args,
-  children: 'Long Primary Args'
+  //children: 'Long Primary Args'
 }
 
 
